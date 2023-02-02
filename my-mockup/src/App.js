928731f2a632ec84e7ce5732b61b3737592a33aa
tamtn2 <77471621+tamtn2@users.noreply.github.com>
2023-02-02 13:38:16 -1000

    Format
    -Changed certain node shapes
    -Changed certain edge type

--- my-mockup/src/App.js
+++ my-mockup/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import data from "./data.json";
 
-import {Sigma, RelativeSize, RandomizeNodePositions} from "react-sigma";
+import {Sigma, RelativeSize, RandomizeNodePositions } from "react-sigma";
 
 function App() {
   return (
-      <Sigma graph={data} settings={{ drawEdges: true, clone: false }}>
+      <Sigma 
+      renderer="canvas" 
+      graph={data} 
+      settings={{ drawEdges: true, clone: false }}>
         <RelativeSize initialSize={5} />
         <RandomizeNodePositions />
       </Sigma>
